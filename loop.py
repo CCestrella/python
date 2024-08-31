a96440ae8363156ad2802ba930406f87bb4948da
Christin Estrella <estrella.cclm@gmail.com>
2024-08-31 10:26:04 +0800

    weather project

---
+++ loop.py
@@ -0,0 +1,29 @@
+# count = 0
+
+# while count <= 10:
+#     count = count + 1
+#     print(count)
+
+# names = ['Alice','Bob','Charlie']
+
+# for name in names:
+#     if name.lower().startswith("3"):
+#         print(name)
+
+# nums = [1, 2, 3, 4.5, 5, 6]
+# squares = []
+
+# for num in nums:
+#     squares.append(num ** 2)
+
+# print(nums)
+# print(squares)
+
+
+# adj = ["red", "big", "tasty"]
+# fruits = ["apple", "banana", "cherry"]
+
+# for x in adj:
+#   for y in fruits:
+#     print(x, y)
+
---
+++ test2.py
@@ -0,0 +1,6 @@
+# from weather import format_temperature, convert_date
+# temp = 37
+# print(format_temperature(temp))
+
+# print(convert_date("2021-07-02T07:00:00+08:00"))
+
